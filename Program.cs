9d20ab2c44a16209b7f09151f27ea72b4b60d1ad
1213-2 <1213-2@1213-2-PC>
2024-01-22 09:12:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace PR_8_VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {    
+                Console.Write("Нажмите Y чтобы продолжить и N чтобы прекратить ");     
+                string select_key = Console.ReadLine();
+
+                switch (select_key)
+                {
+                    case "Y":
+
+                        try
+
+                        {
+                            Console.Title = "Практическая работа № 8"; // заголовок консоли
+                            Console.ForegroundColor = ConsoleColor.Black; // цвет шрифта
+                            Console.BackgroundColor = ConsoleColor.White; // цвет фона шрифта
+                            Console.WriteLine("Здравствуйте!");
+
+                            int a, b;
+
+                            Console.Write("Введите a = ");
+                            a = Int32.Parse(Console.ReadLine());
+                            Console.Write("Введите b = ");
+                            b = Int32.Parse(Console.ReadLine());
+
+                            int stop = (a > b) ? a : b; // проверка условия: если истина - a, ложь - b
+                            int div = 1, max_div = 0;
+
+                            do  // начало цикла do...while
+                            {
+                                if (a % div == 0 && b % div == 0)
+                                {
+                                    max_div = div;
+                                }
+                                div++;
+                            }
+                            while (div <= stop);
+
+                            Console.WriteLine("Наибольший общий делитель " + a + " и " + b + " = " + max_div);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ошибка: введите число, а не cимвол");
+                        }
+
+                        catch (Exception e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ошибка: что-то пошло не так");
+                            Console.WriteLine(e.Message);       
+                        }
+                        Console.ReadLine();
+                        Console.Clear ();
+                        break;
+
+                    case "N":
+                        Console.WriteLine();
+                        Environment.Exit(0);
+                        break;
+                       
+                }
+                  
+            }
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PR 8 VS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR 8 VS")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b4d7da3d-dcf3-449d-9eb0-41cba83b96cd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
